3890cd8c84d3be909535921a79137beef485cfc0
Kagami-yu <yuzi51048@gmail.com>
2025-09-28 23:50:40 +0800

    day07练习

--- day07-code/src/com/itheima/test/test1.java
+++ day07-code/src/com/itheima/test/test1.java
@@ -7,34 +7,30 @@ public class test1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("请输入机票原价");
-        double price = sc.nextDouble();
+        int price = sc.nextInt();
         System.out.println("请输入月份");
         int month = sc.nextInt();
         System.out.println("头等舱0还是经济舱1");
         int seat = sc.nextInt();
 
         if (month >= 5 && month <= 10) {
-            if (seat == 0) {
-                price = price * 0.9;
-            } else if (seat == 1) {
-                price = price * 0.85;
-            } else {
-                System.out.println("无效数据");
-                return;
-            }
+            price=getPrice(price,seat,0.9,0.85);
+            System.out.println(price);
         } else if ((month >= 11 && month <= 12) || (month >= 1 && month <= 4)) {
-            if (seat == 0) {
-                price = price * 0.7;
-            } else if (seat == 1) {
-                price = price * 0.65;
-            } else {
-                System.out.println("无效数据");
-                return;
-            }
+            price=getPrice(price,seat,0.7,0.65);
+            System.out.println(price);
         } else {
             System.out.println("无效数据");
-            return;
         }
-        System.out.println(price);
+    }
+    public static int getPrice(int price,int seat,double num1,double num2){
+        if (seat == 0) {
+            price =(int) (price * num1);
+        } else if (seat == 1) {
+            price =(int) (price * num2);
+        } else {
+            System.out.println("无效数据");
+        }
+        return price;
     }
 }
--- day07-code/src/com/itheima/test/test2.java
+++ day07-code/src/com/itheima/test/test2.java
@@ -1,5 +1,26 @@
 package com.itheima.test;
 
+
+//找101-200间的质数，并且输出所有的质数
 public class test2 {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        //假定一个数为质数
+        //boolean flag = true;要循环得放到for里面
+        int count = 0;
+        for (int i = 101; i < 200; i++) {
+            boolean flag = true;
+            //第一次误写成j<200,导致每个i都可能除到自己被判断成非质数
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                count++;
+                System.out.println("质数" + i);
+            }
+        }
+        System.out.println(count);
+    }
 }
