9aebe7844e7a2c3a4b664e7e0fcf8dea9b379d94
Kagami-yu <yuzi51048@gmail.com>
2025-10-18 22:19:40 +0800

    day11练习

--- day11-code/src/com/itheima/ArrayListTest/Test2.java
+++ day11-code/src/com/itheima/ArrayListTest/Test2.java
@@ -1,4 +1,23 @@
 package com.itheima.ArrayListTest;
 
+import java.util.ArrayList;
+
+//添加学生对象并遍历
 public class Test2 {
+    public static void main(String[] args) {
+        ArrayList<Student> list = new ArrayList<Student>();
+        //创建学生对象
+        Student stu1=new Student("zhangsan",19);
+        Student stu2=new Student("zhangsi",20);
+        Student stu3=new Student("zhangbo",20);
+        //添加到集合中
+        list.add(stu1);
+        list.add(stu2);
+        list.add(stu3);
+        //遍历
+        for (int i = 0; i < list.size(); i++) {
+            Student stu=list.get(i);
+            System.out.println(stu.getName()+","+stu.getAge());
+        }
+    }
 }
