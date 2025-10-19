fbcc0a78b7bcc98b85c6a02e18a2080d13d27b19
Kagami-yu <yuzi51048@gmail.com>
2025-10-19 11:24:10 +0800

    day11练习

---
+++ day11-code/src/com/itheima/ArrayListTest/Test4/UserJudgeTest.java
@@ -0,0 +1,31 @@
+package com.itheima.ArrayListTest.Test4;
+
+import java.util.ArrayList;
+
+public class UserJudgeTest {
+    public static void main(String[] args) {
+        ArrayList<User> userlist = new ArrayList<>();//创建集合
+
+        User u1 = new User("1","zhangsan","123456");
+        User u2 = new User("2","zhangsi","654321");
+        User u3 = new User("3","zhangwu","112233");
+
+        userlist.add(u1);
+        userlist.add(u2);
+        userlist.add(u3);
+
+        boolean flag=check(userlist,"6");
+        System.out.println(flag);
+    }
+
+
+    //
+    public static boolean check(ArrayList<User> userlist,String id){
+        for (int i = 0; i < userlist.size(); i++) {
+            if(userlist.get(i).getId().equals(id)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
