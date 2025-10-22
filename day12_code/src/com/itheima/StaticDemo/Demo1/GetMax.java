f601e3b694261b21a693adab41edc1f07f00ada0
Kagami-yu <yuzi51048@gmail.com>
2025-10-22 23:56:09 +0800

    day12练习

--- day12_code/src/com/itheima/StaticDemo/Demo1/GetMax.java
+++ day12_code/src/com/itheima/StaticDemo/Demo1/GetMax.java
@@ -6,9 +6,11 @@ public class GetMax {
     private GetMax(){}
     public static int getMax(ArrayList<Student> list){
         int max = list.get(0).getAge();
+//        list.get(i).getAge()效率低，进行循环要调用两次，用临时变量接收
         for(int i=1;i<list.size();i++){
-            if(max<list.get(i).getAge()){
-                max = list.get(i).getAge();
+            int tempAge=list.get(i).getAge();
+            if(max< tempAge){
+                max =  tempAge;
             }
         }
         return max;
