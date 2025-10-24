b7214be57bedf979f0ace16a27d65dcff1b3317c
Kagami-yu <yuzi51048@gmail.com>
2025-10-24 23:51:12 +0800

    day12练习

--- day12_code/src/com/itheima/polymorphic/Test4/test4.java
+++ day12_code/src/com/itheima/polymorphic/Test4/test4.java
@@ -4,7 +4,7 @@ public class test4 {
     public static void main(String[] args) {
         //创建对象并调用方法
         Person p1=new Person("老李",45);
-        Person p2=new Person("老刘",46);
+        Person p2=new Person("老于",46);
         Dog d=new Dog(2,"黄");
         Cat c=new Cat(1,"白");
         p1.keepPet(d,"骨头");
