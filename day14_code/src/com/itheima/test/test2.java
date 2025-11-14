04fa635855382043fb78848110e5a9b28041e1e3
Kagami-yu <yuzi51048@gmail.com>
2025-11-14 21:18:46 +0800

    day14练习

--- day14_code/src/com/itheima/test/test2.java
+++ day14_code/src/com/itheima/test/test2.java
@@ -4,18 +4,25 @@ import java.util.Scanner;
 
 public class test2 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("请输入整数");
-        String str=sc.nextLine();
-        int num=0;
+        String str = sc.nextLine();
+        int num = 0;
+        int count = 0;
         char[] charArray = str.toCharArray();
         for (int i = 0; i < charArray.length; i++) {
-            if(charArray[i]=='0') {
+            if (charArray[0] == '0') {
                 System.out.println("数据不规范");
                 return;
-            }else{
-                num = num * 10 + (charArray[i] - 48);
             }
+            if (count == 10) {
+                System.out.println("数据长度超出");
+                return;
+            } else {
+                num = num * 10 + (charArray[i] - 48);
+                count++;
+            }
+
         }
         System.out.println(num);
     }
