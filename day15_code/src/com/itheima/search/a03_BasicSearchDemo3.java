a3d929174380351c93b336a8b5332dba4f2bc6c3
Kagami-yu <yuzi51048@gmail.com>
2025-11-15 14:08:40 +0800

    day15练习

---
+++ day15_code/src/com/itheima/search/a03_BasicSearchDemo3.java
@@ -0,0 +1,15 @@
+package com.itheima.search;
+
+public class a03_BasicSearchDemo3 {
+    public static void main(String[] args) {
+        /*
+        二分查表改进
+        1.插值查找
+        mid=min+(key-arr[min]/arr[max]-arr[min])*(max-min)
+        ====适用于数值分布较为均匀的时候求mid值，数值不均匀反倒不如普通的二分查找
+        2.斐波那契查找
+        黄金分割比1:0.618
+        mid=min+黄金分割点左半边长度-1
+         */
+    }
+}
